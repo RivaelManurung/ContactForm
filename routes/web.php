d9ab3054da46759a09393f60faabfafdac33eac6
Rivael <135939684+RivaelManurung@users.noreply.github.com>
2025-10-04 14:04:19 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 use App\Http\Controllers\ContactController;
 
-Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
-Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
+Route::get('/', [ContactController::class, 'showForm'])->name('contact.form');
+Route::post('/', [ContactController::class, 'submitForm'])->name('contact.submit');
